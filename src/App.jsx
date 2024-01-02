c3e5f440199a84947f86cbc1167a0f57a56158f3
h-zahar <hz.rifat@gmail.com>
2024-01-02 12:35:43 +0600

    Implemented Folder Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const folders = {
+    folder1: {},
+    folder2: {},
+    folder3: {
+      sub1: {},
+      sub2: {
+        subsub1: {},
+      },
+    },
+  };
+
+  const handleAccess = (e) => {
+    // if (clicked.length === 0) {
+    // console.log(Object.keys(renderedFolders[e.target.value]));
+    // setClicked(e.target.value);
+    setRenderedFolders(renderedFolders[e.target.value]);
+    // }
+  };
+
+  // const [clicked, setClicked] = useState("");
+  // console.log("tar", clicked);
+
+  const [renderedFolders, setRenderedFolders] = useState(folders);
+
   return (
     <>
-      <p>Let{"'"}s Start!!</p>
+      <h4>Manage your Folders</h4>
+      {Object.keys(renderedFolders).length === 0 && <p>No folder found!</p>}
+      {Object.keys(renderedFolders).map((f) => {
+        return (
+          <button key={f} value={f} onClick={handleAccess}>
+            {f}
+          </button>
+        );
+      })}
     </>
   );
 }
